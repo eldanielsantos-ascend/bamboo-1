8f878f3804c67a9a8a9521c721fa8ca6a227aea7
eldanielsantos-ascend <eldanielsantos@gmail.com>
2024-12-21 12:40:49 -0300

    Initial commit

---
+++ src/page/index.tsx
@@ -0,0 +1,122 @@
+import React from "react";
+import "./index.css";
+
+export default function Main() {
+  return (
+    <div className="main-container">
+      <div className="flex-column-cb">
+        <div className="captura-de-tela" />
+        <span className="lorem-ipsum">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida.
+          <br />
+          <br />
+          <br />
+          Risus commodo viverra maecenas accumsan lacus vel facilisis.{" "}
+        </span>
+        <div className="rectangle" />
+        <span className="clientes-confiam-gente">
+          Diversos clientes que já confiam na gente
+        </span>
+        <div className="layer-copy" />
+        <span className="banco-videos-personalizado">
+          Tenha um
+          <br />
+          banco de vídeos personalizado
+          <br />
+          para sua marca.
+        </span>
+        <div className="rectangle-1">
+          <div className="layer">
+            <div className="layer-2" />
+            <div className="layer-3">
+              <div className="layer-4" />
+            </div>
+          </div>
+        </div>
+        <div className="rectangle-copy" />
+        <span className="lorem-ipsum-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida.
+          <br />
+          <br />
+          <br />
+          Risus commodo viverra maecenas accumsan lacus vel facilisis.{" "}
+        </span>
+        <div className="rectangle-copy-5">
+          <div className="swell">
+            <div className="swell-6" />
+          </div>
+        </div>
+        <div className="rectangle-7">
+          <span className="quero-conhecer-mais">Quero conhecer mais</span>
+        </div>
+        <div className="rectangle-copy-8">
+          <div className="layer-9">
+            <div className="layer-a" />
+          </div>
+        </div>
+        <div className="rectangle-copy-b" />
+        <div className="rectangle-copy-c">
+          <div className="layer-d">
+            <div className="layer-e" />
+          </div>
+        </div>
+        <div className="rectangle-f" />
+        <div className="bamboo-studios-10">
+          <span className="bamboo-studios-11">
+            Bamboo Studios
+            <br />
+          </span>
+          <span className="voce-pensa-agente-cria">
+            Você pensa. A gente cria.
+          </span>
+        </div>
+        <div className="rectangle-copy-12" />
+        <div className="rectangle-copy-13" />
+        <div className="processo-unico-producao">
+          <span className="processo-unico-producao-14">
+            Desenvolvemos um processo único de produção para
+            <br />
+          </span>
+          <span className="budgets-mais-eficientes">
+            budgets mais eficientes.
+          </span>
+        </div>
+        <div className="layer-15" />
+        <div className="rectangle-16">
+          <div className="flex-column-fc">
+            <span className="qualidade">Qualidade</span>
+            <span className="team-filmmakers-brazilian">
+              Orgulhosamente apresentamos nosso time de filmmakers brasileiros.{" "}
+              <br />
+              Conheça um pouco da história de cada um deles e licencie suas
+              imagens
+              <br />
+              <br />
+              Orgulhosamente apresentamos nosso time <br />
+              de filmmakers brasileiros. Conheça um poucoOrgulhosamente
+              apresentamos nosso time de filmmakers brasileiros. Conheça um
+              pouco
+              <br />
+              da história de cada um deles e licencie suas imagens
+              <br />
+              da história de cada um deles e licencie suas imagens
+            </span>
+          </div>
+          <div className="k-fhd">
+            <span className="k">4k</span>
+            <span className="resolution"> 3048x2160 & </span>
+            <span className="k-17">FHD</span>
+            <span className="resolution-18"> 1920x1080</span>
+          </div>
+        </div>
+      </div>
+      <div className="rectangle-copy-19" />
+      <div className="layer-1a" />
+      <div className="layer-1b" />
+    </div>
+  );
+}
